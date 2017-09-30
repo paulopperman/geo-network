8ca31aad89e51fa35fd5e95be1d0da25e0d2569e
paulopperman <paul.opperman@gmail.com>
2017-09-30 12:23:15 -0400

    store edges as list of tuples with length parameter

--- shp_to_networkx.py
+++ shp_to_networkx.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 import shapely as sh
 import networkx as nx
+import numpy as np
 
 file = "./test-geometry/test-geometry.shp"
 
@@ -10,7 +11,8 @@ buffersize = .00000000005  # set the buffer around the line to search for inters
 gdf = gpd.read_file(file)
 
 # add column for the subgraph of each linestring
-gdf['graph'] = gdf.apply(lambda x: {}, axis=1)
+gdf['nodes'] = gdf.apply(lambda x: {}, axis=1)
+gdf['edges'] = np.empty((len(gdf), 0)).tolist()
 
 # initialize the main graph
 G = nx.Graph()
@@ -24,7 +26,9 @@ for m in range(0, len(gdf)):
     # collect base nodes
     pts = list(gdf.iloc[m].geometry.coords)
     for p in pts:
-        gdf.graph.iloc[m][next_node] = sh.geometry.Point(p)  # TODO: streamline with a placeholder graph so we're not selecting from gdf every time
+        point = sh.geometry.Point(p)
+        dist = s.geometry.project(point)
+        gdf.nodes.iloc[m][next_node] = {'point':point,'pos':dist}  # TODO: streamline with a placeholder graph so we're not selecting from gdf every time
         next_node = next_node+1
 
     for n in range(m+1, len(gdf)):  # iterate through all the upcoming lines
@@ -45,11 +49,17 @@ for m in range(0, len(gdf)):
                 o_pt = o.representative_point()  # FIXME: this will result in some weird and close node placements if intersection is an endpoint (clean up big graph?)
                 o_proj = s.geometry.project(o_pt)
                 int_point = s.geometry.interpolate(o_proj)
-                gdf.graph.iloc[m][next_node] = int_point
-                gdf.graph.iloc[n][next_node] = int_point
+                gdf.nodes.iloc[m][next_node] = {'point':int_point, 'pos':o_proj}
+                gdf.nodes.iloc[n][next_node] = {'point':int_point, 'pos':t.geometry.project(o_pt)}
                 next_node = next_node+1
 
-    # compute edge distances
+    # build edge array of tuples
+    subg = gpd.GeoDataFrame.from_dict(data=gdf.nodes.iloc[m],orient='index')  # load dict as geodataframe for processing
+    ordered_points = list(subg.sort_values(by='pos', axis=0).index)  # get list of nodes sorted by position
+    for r in range(0, len(ordered_points)-1):
+        edge_length = subg.iloc[r]['point'].distance(subg.iloc[r+1]['point'])  # distance to the next point
+        gdf.iloc[m].edges.append((ordered_points[r],ordered_points[r+1],{'length':edge_length}))
+
 
 
 
